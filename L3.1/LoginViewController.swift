99a986a756be6676779935be4662bdebf06f2f5a
mikle <mikle.kontinent@gmail.com>
2025-06-06 00:01:08 +0700

    final design and start working on code

---
+++ L3.1/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  L3.1
+//
+//  Created by Таня Кожевникова on 05.06.2025.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: -IB Outlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Private properties
+    private let user = "User"
+    private let password = "Password"
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
